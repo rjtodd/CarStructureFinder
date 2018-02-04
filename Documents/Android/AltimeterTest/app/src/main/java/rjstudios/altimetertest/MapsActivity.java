4565ca80faa9c28e3249e97e95b3c0560dd8b37c
rjtodd <ronan.jtodd@gmail.com>
2018-02-04 10:49:22 -0800

    Push

--- Documents/Android/AltimeterTest/app/src/main/java/rjstudios/altimetertest/MapsActivity.java
+++ Documents/Android/AltimeterTest/app/src/main/java/rjstudios/altimetertest/MapsActivity.java
@@ -1,14 +1,27 @@
 package rjstudios.altimetertest;
 
+import android.*;
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.PendingIntent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,8 +30,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements GoogleMap.OnMyLocationButtonClickListener,
-        GoogleMap.OnMyLocationClickListener, OnMapReadyCallback {
+import java.io.IOException;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
@@ -27,8 +42,17 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMyLoca
     private static final String KEY_LOCATION = "location";
 
     private GoogleMap mMap;
-    private Location mLastKnownLocation;
+
+    private Location mLastKnownLocation, mLocation;
+    private LocationManager manager;
+    private LocationListener locationListener;
+
     private boolean mLocationPermissionGranted;
+    private boolean mRequestingLocationUpdates;
+    private LocationCallback mLocationCallback;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest mLocationRequest;
+    private PendingIntent mPendingIntent;
 
     @SuppressLint("MissingPermission")
     @Override
@@ -38,11 +62,103 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMyLoca
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        manager = (LocationManager) getSystemService(LOCATION_SERVICE); // get location service
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                //get lat long  from location
+                mMap.clear();
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                Geocoder geocoder = new Geocoder(getApplicationContext());
+                /*List<Address> addresses =
+                        geocoder.getFromLocation(latitude, longitude, 1);
+                String result = addresses.get(0).getSubLocality()+":";
+                result += addresses.get(0).getLocality()+":";
+                result += addresses.get(0).getCountryCode();*/
+                LatLng latLng = new LatLng(latitude, longitude);
+                mMap.addMarker(new MarkerOptions().position(latLng).title("marker"));
+                mMap.setMaxZoomPreference(20);
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+       /* if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);*/
+            /* LatLng sydney = new LatLng(-34, 151);
+            mMap.addMarker(new MarkerOptions()
+                    .position(sydney).title("Marker"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        manager.removeUpdates(locationListener);
+        Log.i("onPause...","paused");
+    }
+
+    protected void createLocationRequest() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+    }
+
+    private void startLocationUpdates() {
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -53,14 +169,8 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMyLoca
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        mMap.setMyLocationEnabled(true);
-        mMap.setOnMyLocationButtonClickListener(this);
-        mMap.setOnMyLocationClickListener(this);
-            /* LatLng sydney = new LatLng(-34, 151);
-            mMap.addMarker(new MarkerOptions()
-                    .position(sydney).title("Marker"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
-        }
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mPendingIntent);
+    }
 
     /**
      * Saves the state of the map when the activity is paused.
@@ -92,21 +202,9 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMyLoca
         //updateLocationUI();
     }
 
-    @Override
-    public void onMyLocationClick(@NonNull Location location) {
-        LatLng marker = new LatLng(location.getLatitude(),location.getLongitude());
-        mMap.addMarker(new MarkerOptions()
-                .position(marker).title("Marker"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker));
-    }
-    @Override
-    public boolean onMyLocationButtonClick() {
-        Toast.makeText(this,"MyLocation button clicked", Toast.LENGTH_LONG).show();
-        return false;
-    }
 
 
-        /**
+    /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
